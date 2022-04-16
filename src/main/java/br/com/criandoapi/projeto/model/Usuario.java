ff211953cec48051cfbd65acc2f37fb2bbd87ad3
daniel.silveira <daniel.repres@hotmail.com>
2022-04-15 23:06:14 -0300

    Ok

--- src/main/java/br/com/criandoapi/projeto/model/Usuario.java
+++ src/main/java/br/com/criandoapi/projeto/model/Usuario.java
@@ -7,6 +7,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
+
 @Entity
 @Table(name = "usuario")
 public class Usuario {
@@ -27,36 +31,4 @@ public class Usuario {
 	
 	@Column(name = "telefone", length = 15, nullable = true)
 	private String telefone;
-	
-	
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
 }
