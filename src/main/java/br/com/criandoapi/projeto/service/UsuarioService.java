c94eb0b2f3be0b4345b2516a9c63dc6886a4104a
daniel.silveira <daniel.repres@hotmail.com>
2022-04-17 11:14:54 -0300

    ok

--- src/main/java/br/com/criandoapi/projeto/service/UsuarioService.java
+++ src/main/java/br/com/criandoapi/projeto/service/UsuarioService.java
@@ -14,37 +14,36 @@ import br.com.criandoapi.projeto.repository.IUsuario;
 import br.com.criandoapi.projeto.security.Token;
 import br.com.criandoapi.projeto.security.TokenUtil;
 
-
 @Service
 public class UsuarioService {
-		
+
 	private IUsuario repository;
 	private PasswordEncoder passwordEncoder;
-	
+
 	public UsuarioService(IUsuario repository) {
 		this.repository = repository;
 		this.passwordEncoder = new BCryptPasswordEncoder();
 	}
-	
+
 	public List<Usuario> listarUsuario() {
 		List<Usuario> lista = repository.findAll();
 		return lista;
 	}
-	
+
 	public Usuario criarUsuario(Usuario usuario) {
 		String encoder = this.passwordEncoder.encode(usuario.getSenha());
 		usuario.setSenha(encoder);
 		Usuario usuarioNovo = repository.save(usuario);
 		return usuarioNovo;
 	}
-	
+
 	public Usuario editarUsuario(Usuario usuario) {
 		String encoder = this.passwordEncoder.encode(usuario.getSenha());
 		usuario.setSenha(encoder);
 		Usuario usuarioNovo = repository.save(usuario);
 		return usuarioNovo;
 	}
-	
+
 	public Boolean excluirUsuario(Integer id) {
 		repository.deleteById(id);
 		return true;
@@ -58,13 +57,12 @@ public class UsuarioService {
 
 	public Token gerarToken(@Valid UsuarioDto usuario) {
 		Usuario user = repository.findBynomeOrEmail(usuario.getNome(), usuario.getEmail());
-		if(user != null) {
+		if (user != null) {
 			Boolean valid = passwordEncoder.matches(usuario.getSenha(), user.getSenha());
-			if(valid) {
+			if (valid) {
 				return new Token(TokenUtil.createToken(user));
 			}
 		}
 		return null;
 	}
-
 }
